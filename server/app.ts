dd5fa6a15bc018c9c8fd5aae24eac12767395458
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-04-30 12:34:24 +0200

    set up routes and controller structure, set up app to use routes and added error handling middleware

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,14 @@
 import express from 'express';
+import moviesRouter from './routes/movies-router';
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use('/movies', moviesRouter);
+app.use((err:Error, req:express.Request, res:express.Response, next:express.NextFunction) => {
+    res.status(500).json({message: err.message});
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
     });
